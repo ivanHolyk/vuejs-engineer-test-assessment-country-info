1dc2e029dbe9c6fc290a61f7a3e7f7f2f253f424
ivan_holyk <ivan.Holyk369@gmail.com>
2024-09-25 11:16:54 +0300

    add bootstrap5

--- src/components/HolidayComponent.vue
+++ src/components/HolidayComponent.vue
@@ -2,6 +2,8 @@
   {{ props.holiday.name }}, {{ props.holiday.localName }}
   <br />
   {{ props.holiday.date }}
+  <br />
+  <span v-for="type in props.holiday.types" class="badge text-bg-secondary">{{ type }}</span>
 </template>
 
 <script setup>
